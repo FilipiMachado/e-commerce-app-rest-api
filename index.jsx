d552e7864f4791d1607e7d3cae0e946a6a9cb848
Filipi <filipisee@hotmail.com>
2021-11-17 22:44:01 -0300

    added mongodb connection

--- index.jsx
+++ index.jsx
@@ -1,2 +1,13 @@
-console.log("Hello World!");
+// Express
+const express = require("express");
+const app = express();
+// MongoDB
+const mongoose = require("mongoose");
 
+mongoose.connect(
+  "mongodb+srv://fil:123654@e-commerce-api.pub8n.mongodb.net/shop?retryWrites=true&w=majority"
+);
+
+app.listen(5000, () => {
+  console.log("Server Backend bombando!");
+});
