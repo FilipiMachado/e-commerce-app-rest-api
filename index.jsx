18fc0d3e55460d3c031f8a66d03a3a2d1cca5b6a
Filipi <filipisee@hotmail.com>
2021-11-17 22:49:29 -0300

    some changes

--- index.jsx
+++ index.jsx
@@ -3,10 +3,19 @@ const express = require("express");
 const app = express();
 // MongoDB
 const mongoose = require("mongoose");
+// Dotenv
+const dotenv = require("dotenv");
 
-mongoose.connect(
-  "mongodb+srv://fil:123654@e-commerce-api.pub8n.mongodb.net/shop?retryWrites=true&w=majority"
-);
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("DBConnection ta bagual!");
+  })
+  .catch(() => {
+    console.log("Deu ruim!");
+  });
 
 app.listen(5000, () => {
   console.log("Server Backend bombando!");
