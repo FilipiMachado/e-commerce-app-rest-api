9a8714b8d2e0b0edb6dc793f968bc35c9ea1656d
Filipi <filipisee@hotmail.com>
2021-11-19 23:49:41 -0300

    adding logic to auth.js route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,21 @@
 const router = require("express").Router();
+// Model
+const User = require("../models/User");
 
 // REGISTER
-router.post("/register", (req, res) => {
-    
-})
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
