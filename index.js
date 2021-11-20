20a03f68e8b1ae7bc7d4cad9c22dba8784a9f1bb
Filipi <filipisee@hotmail.com>
2021-11-19 23:59:16 -0300

    adding logic to auth.js route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 // Routes
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ mongoose
 
 // User Routes
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(5000, () => {
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 
 // REGISTER
-router.post("/register", async (req, res) => {
+router.post("/register", async (req, res) => {  
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
