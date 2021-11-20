95201a796877649d4c5da20d5cf8a6151d71a913
Filipi <filipisee@hotmail.com>
2021-11-19 22:08:21 -0300

    updating routes

--- routes/user.jsx
+++ routes/user.jsx
@@ -4,4 +4,10 @@ router.get("/usertest", (req, res) => {
   res.send("user test is successful!");
 });
 
+router.post("userpostest"),
+  (req, res) => {
+    const username = req.body.username;
+    console.log(username);
+  };
+
 module.exports = router;
