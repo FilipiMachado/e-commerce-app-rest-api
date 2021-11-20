6b937f4b61db5841c0938054dcea935a1ee5684c
Filipi <filipisee@hotmail.com>
2021-11-19 23:39:11 -0300

    adding new routes and models

---
+++ models/Product.js
@@ -0,0 +1,17 @@
+// Mongoose
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    desc: { type: String, required: true },
+    img: { type: String, required: true },
+    categories: { type: Array },
+    size: { type: String },
+    color: { type: String },
+    price: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", ProductSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 
-
+// REGISTER
+router.post("/register", (req, res) => {
+    
+})
 
 module.exports = router;
